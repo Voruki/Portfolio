ac3600c5b2aea92df71cd0bac259b5536e6d2b23
Vorky <98268785+Voruki@users.noreply.github.com>
2026-04-10 01:04:29 +0800

    Update HomeComponent.js

--- src/pages/home/HomeComponent.js
+++ src/pages/home/HomeComponent.js
@@ -9,10 +9,15 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <Header theme={this.props.theme} />
+        {/* MECHANICAL FIX: Pass the onToggle function here */}
+        <Header theme={this.props.theme} onToggle={this.props.onToggle} />
+        
         <Greeting theme={this.props.theme} />
         <Skills theme={this.props.theme} />
-        <Footer theme={this.props.theme} />
+        
+        {/* Optional: Pass it to Footer if your footer has a theme-dependent logo */}
+        <Footer theme={this.props.theme} onToggle={this.props.onToggle} />
+        
         <TopButton theme={this.props.theme} />
       </div>
     );
