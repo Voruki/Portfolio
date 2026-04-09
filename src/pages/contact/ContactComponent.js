b5ab4bbabe9dfdea4cb4c6c6b250a19b3bbfd373
Vorky <98268785+Voruki@users.noreply.github.com>
2026-04-10 00:19:53 +0800

    Update ContactComponent.js

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -4,14 +4,12 @@ import Footer from "../../components/footer/Footer";
 import TopButton from "../../components/topButton/TopButton";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
-import BlogsImg from "./BlogsImg";
 import AddressImg from "./AddressImg";
 import { Fade } from "react-reveal";
 import "./ContactComponent.css";
-import { greeting, contactPageData } from "../../portfolio.js"; // Added greeting import
+import { greeting, contactPageData } from "../../portfolio.js";
 
 const ContactData = contactPageData.contactSection;
-const blogSection = contactPageData.blogSection;
 const addressSection = contactPageData.addressSection;
 const phoneSection = contactPageData.phoneSection;
 
@@ -27,7 +25,7 @@ class Contact extends Component {
               <div className="contact-heading-img-div">
                 <img
                   src={require(`../../assets/images/${ContactData["profile_image_path"]}`)}
-                  alt=""
+                  alt="Profile"
                   style={{
                     borderRadius: "50%",
                     width: "350px",
@@ -54,43 +52,15 @@ class Contact extends Component {
                   <Button
                     text="See My Resume"
                     newTab={true}
-                    href={greeting.resumeLink} // MECHANICAL FIX: Pointing to Google Drive
+                    href={greeting.resumeLink}
                     theme={theme}
                   />
                 </div>
               </div>
             </div>
           </Fade>
-          
-          {/* Blog Section (Hidden if title is empty in portfolio.js) */}
-          {blogSection.title && (
-            <Fade bottom duration={1000} distance="40px">
-              <div className="blog-heading-div">
-                <div className="blog-heading-text-div">
-                  <h1 className="blog-heading-text" style={{ color: theme.text }}>
-                    {blogSection["title"]}
-                  </h1>
-                  <p
-                    className="blog-header-detail-text subTitle"
-                    style={{ color: theme.secondaryText }}
-                  >
-                    {blogSection["subtitle"]}
-                  </p>
-                  <div className="blogsite-btn-div">
-                    <Button
-                      text="Visit My Blogsite"
-                      newTab={true}
-                      href={blogSection.link}
-                      theme={theme}
-                    />
-                  </div>
-                </div>
-                <div className="blog-heading-img-div">
-                  <BlogsImg theme={theme} />
-                </div>
-              </div>
-            </Fade>
-          )}
+
+          {/* Blog Section and BlogsImg have been purged from here */}
 
           <Fade bottom duration={1000} distance="40px">
             <div className="address-heading-div">
