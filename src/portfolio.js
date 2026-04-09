734041b916093fb3c58d0963885ce765259bc147
Vorky <98268785+Voruki@users.noreply.github.com>
2026-04-09 15:03:05 +0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,43 +26,34 @@ const greeting = {
     "APAC Presales Solution Manager at Libiao Robotics & First-Class Mechanical Engineer (3.9 CGPA). I architect bespoke automation for SEAO, delivering 40-60% efficiency gains through T-Sort & AirRob systems.",
   resumeLink:
     "https://drive.google.com/file/d/1Wl2DxDzZ2yQiQ_c0S2T6LpCYms2869VN/view",
-  portfolio_repository: "https://github.com/Voruki/Portfolio",
+  portfolio_repository: "https://github.com/Voruki/voruki.github.io",
   githubProfile: "https://github.com/Voruki",
 };
 
 const socialMediaLinks = [
-  /* Your Social Media Link */
-  // github: "https://github.com/ashutosh1919",
-  // linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
-  // gmail: "ashutoshhathidara98@gmail.com",
-  // gitlab: "https://gitlab.com/ashutoshhathidara98",
-  // facebook: "https://www.facebook.com/laymanbrother.19/",
-  // twitter: "https://twitter.com/ashutosh_1919",
-  // instagram: "https://www.instagram.com/layman_brother/"
-
   {
     name: "LinkedIn",
     link: "https://www.linkedin.com/in/ngohyongming/",
-    fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
-    backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+    fontAwesomeIcon: "fa-linkedin-in",
+    backgroundColor: "#0077B5",
   },
   {
     name: "Github",
     link: "https://github.com/Voruki",
-    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
-    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+    fontAwesomeIcon: "fa-github",
+    backgroundColor: "#181717",
   },
   {
     name: "Instagram",
     link: "https://www.instagram.com/voruki_studio/",
-    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
-    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+    fontAwesomeIcon: "fa-instagram",
+    backgroundColor: "#E4405F",
   },
   {
     name: "Gmail",
     link: "mailto:yongming914@gmail.com",
-    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
-    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+    fontAwesomeIcon: "fa-google",
+    backgroundColor: "#D14836",
   }
 ];
 
@@ -73,31 +64,24 @@ const skills = {
       fileName: "CloudInfraImg",
       skills: [
         "⚡ Spearheading the first AirRob & T-Sort implementations across Thailand and Malaysia",
-        "⚡ Architecting bespoke solutions delivering the average of 40-60% operational efficiency gains",
+        "⚡ Architecting bespoke solutions delivering average 40-60% operational efficiency gains",
         "⚡ Leading technical lifecycles from site audits to finalized system design architecture",
       ],
       softwareSkills: [
         {
           skillName: "AutoCAD",
           fontAwesomeClassname: "simple-icons:autodesk",
-          style: {
-            backgroundColor: "transparent",
-          },
+          style: { backgroundColor: "transparent" },
         },
         {
           skillName: "ZWCAD",
           fontAwesomeClassname: "simple-icons:keras",
-          style: {
-            backgroundColor: "white",
-            color: "#D00000",
-          },
+          style: { backgroundColor: "white", color: "#D00000" },
         },
         {
           skillName: "Internal Simulation Tools",
           fontAwesomeClassname: "simple-icons:ansys",
-          style: {
-            backgroundColor: "transparent",
-          },
+          style: { backgroundColor: "transparent" },
         }
       ],
     },
@@ -113,23 +97,17 @@ const skills = {
         {
           skillName: "Blender",
           fontAwesomeClassname: "simple-icons:blender",
-          style: {
-            color: "#F5792A",
-          },
+          style: { color: "#F5792A" },
         },
         {
           skillName: "SketchUp",
           fontAwesomeClassname: "simple-icons:sketchup",
-          style: {
-            color: "#005F9E",
-          },
+          style: { color: "#005F9E" },
         },
         {
           skillName: "DaVinci Resolve",
           fontAwesomeClassname: "simple-icons:davinciresolve",
-          style: {
-            color: "#1D2A47",
-          },
+          style: { color: "#1D2A47" },
         }
       ],
     },
@@ -145,30 +123,22 @@ const skills = {
         {
           skillName: "Microsoft Project",
           fontAwesomeClassname: "simple-icons:microsoftproject",
-          style: {
-            color: "#31752f",
-          },
+          style: { color: "#31752f" },
         },
         {
           skillName: "Google Analytics",
           fontAwesomeClassname: "simple-icons:googleanalytics",
-          style: {
-            color: "#E37400",
-          },
+          style: { color: "#E37400" },
         },
         {
           skillName: "Python",
           fontAwesomeClassname: "logos-python",
-          style: {
-            color: "transparent",
-          },
+          style: { color: "transparent" },
         },
         {
           skillName: "Office 365",
           fontAwesomeClassname: "simple-icons:microsoftoffice",
-          style: {
-            color: "#D83B01",
-          },
+          style: { color: "#D83B01" },
         },
       ],
     },
@@ -177,30 +147,24 @@ const skills = {
       fileName: "FullStackImg",
       skills: [
         "⚡ Overseeing technical governance from conceptual BOM to pre-execution infrastructure",
-        "⚡ Finalizing Network Topology, Electrical Diagrams, and Technical Agreements for zero-error handovers",
-        "⚡ Conducting high-frequency site audits to engineer optimal warehouse design modifications",
+        "⚡ Finalizing Network Topology, Electrical Diagrams, and Technical Agreements",
+        "⚡ Conducting high-frequency site audits to engineer optimal warehouse designs",
       ],
       softwareSkills: [
         {
           skillName: "Mechanical Design",
           fontAwesomeClassname: "simple-icons:blueprint",
-          style: {
-            color: "#000000",
-          },
+          style: { color: "#000000" },
         },
         {
           skillName: "Simulation Tools",
           fontAwesomeClassname: "simple-icons:analogue",
-          style: {
-            color: "#61DAFB",
-          },
+          style: { color: "#61DAFB" },
         },
         {
           skillName: "Infrastructure",
           fontAwesomeClassname: "simple-icons:serverfault",
-          style: {
-            color: "#FF9900",
-          },
+          style: { color: "#FF9900" },
         },
       ],
     },
@@ -211,27 +175,15 @@ const skills = {
 const competitiveSites = {
   competitiveSites: [
     {
-      siteName: "LeetCode",
-      iconifyClassname: "simple-icons:leetcode",
-      style: {
-        color: "#F79F1B",
-      },
-      profileLink: "https://leetcode.com/Voruki/",
-    },
-    {
       siteName: "IEM Malaysia",
       iconifyClassname: "simple-icons:engineering",
-      style: {
-        color: "#000000",
-      },
+      style: { color: "#000000" },
       profileLink: "https://www.myiem.org.my/",
     },
     {
       siteName: "CIDB Malaysia",
       iconifyClassname: "simple-icons:construction",
-      style: {
-        color: "#009B4D",
-      },
+      style: { color: "#009B4D" },
       profileLink: "https://www.cidb.gov.my",
     },
   ],
@@ -247,7 +199,7 @@ const degrees = {
       duration: "2018 - 2022",
       descriptions: [
         "⚡ Achieved First-Class Honours with a CGPA of 3.9/4.0",
-        "⚡ Consecutively awarded the Dean’s Honours List for every semester throughout the 4-year tenure, demonstrating sustained academic excellence",
+        "⚡ Consecutively awarded the Dean’s Honours List for every semester",
         "⚡ Specialized in mechanical systems and engineering design principles",
       ],
       website_link: "https://www.ucsiuniversity.edu.my/",
@@ -261,8 +213,7 @@ const certifications = {
       title: "Google Data Analytics",
       subtitle: "Google Professional Certificate",
       logo_path: "google_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/professional-cert/EW3WGO0X3KHF?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=prof",
+      certificate_link: "https://www.coursera.org/account/accomplishments/professional-cert/EW3WGO0X3KHF",
       alt_name: "Google",
       color_code: "#4285F499",
     },
@@ -270,46 +221,17 @@ const certifications = {
       title: "Google Project Management",
       subtitle: "Google Professional Certificate",
       logo_path: "google_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/professional-cert/KHS5MQHILXM2",
+      certificate_link: "https://www.coursera.org/account/accomplishments/professional-cert/KHS5MQHILXM2",
       alt_name: "Google",
       color_code: "#4285F499",
-    },
-    {
-      title: "Microsoft Project Management",
-      subtitle: "Microsoft Professional Certificate",
-      logo_path: "microsoft_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/professional-cert/GSAW1DOXXU7Y",
-      alt_name: "Microsoft",
-      color_code: "#00A4EF99",
-    },
-    {
-      title: "IBM Project Manager",
-      subtitle: "IBM Professional Certificate",
-      logo_path: "ibm_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/professional-cert/OOQRCNJMV7W1",
-      alt_name: "IBM",
-      color_code: "#052FAD99",
     },
     {
       title: "CS50 Introduction to Programming with Python",
       subtitle: "Harvard University",
       logo_path: "harvard_logo.png",
-      certificate_link:
-        "https://certificates.cs50.io/5858ed45-fe4c-4932-9edb-afc616c0ca4c.png?size=letter",
+      certificate_link: "https://certificates.cs50.io/5858ed45-fe4c-4932-9edb-afc616c0ca4c.png",
       alt_name: "Harvard University",
       color_code: "#A51C30",
-    },
-    {
-      title: "Android Certified Application Developer",
-      subtitle: "Android ATC",
-      logo_path: "android_logo.png",
-      certificate_link:
-        "https://androidatc.com/_transcript.php",
-      alt_name: "Android ATC",
-      color_code: "#3DDC8499",
     },
   ],
 };
@@ -318,8 +240,7 @@ const certifications = {
 const experience = {
   title: "Experience",
   subtitle: "Robotics, Automation & Solutions Design",
-  description:
-    "I manage the end-to-end technical lifecycle for major intralogistics projects across the APAC region, delivering data-driven efficiency for global clients.",
+  description: "I manage the end-to-end technical lifecycle for major intralogistics projects across the APAC region.",
   header_image_path: "experience.svg",
   sections: [
     {
@@ -332,9 +253,8 @@ const experience = {
           company_url: "https://libiaorobot.com/en",
           logo_path: "libiao_logo.png",
           duration: "Aug 2025 - Present",
-          location: "Singapore",
-          description:
-            "Leading automation design & solution deployments in Malaysia, Singapore, Thailand, Vietnam, Indonesia, Australia and New Zealand.",
+          location: "Kuala Lumpur, Malaysia",
+          description: "Leading automation design & solution deployments in Malaysia, Singapore, Thailand, Vietnam, Indonesia, Australia and NZ.",
           color: "#EE2C2C",
         },
         {
@@ -344,8 +264,7 @@ const experience = {
           logo_path: "lsi_logo.png",
           duration: "Sep 2021 - Aug 2025",
           location: "Kuala Lumpur, Malaysia",
-          description:
-            "Integrated ASRS and AutoStore systems, achieving a 30% improvement in space utilization for major DC projects",
+          description: "Integrated ASRS and AutoStore systems, achieving a 30% improvement in space utilization.",
           color: "#0879bf",
         },
       ],
@@ -356,14 +275,13 @@ const experience = {
 // Projects Page
 const projectsHeader = {
   title: "Industrial Projects",
-  description:
-    "Engineering high-efficiency automation systems for the APAC region. My work focuses on spearheading the technical lifecycle of intralogistics solutions—from conceptual site audits to the deployment of robotic systems like AirRob and T-Sort.",
+  description: "Engineering high-efficiency automation systems for the APAC region. Focus on robotic deployments like AirRob and T-Sort.",
   avatar_image_path: "projects_image.svg",
 };
 
 const publicationsHeader = {
   title: "Special Projects",
-  description: "A showcase of specialized warehouse design consultations and community-driven engineering initiatives, leveraging high-fidelity 3D simulations and technical throughput modeling.",
+  description: "Showcase of specialized warehouse design consultations and community-driven engineering initiatives.",
   avatar_image_path: "projects_image.svg",
 };
 
@@ -373,22 +291,8 @@ const publications = {
       id: "cold-storage-tech-open-day",
       name: "Cold Storage Technology Open Day: Simulation Project",
       createdAt: "2024-07-06T00:00:00Z",
-      description: "Delivered technical sessions on CAPEX/OPEX modeling for Tier-1 stakeholders. Developed 3D renderings and simulations to visualize operational flow and pallet capacity.",
+      description: "Technical sessions on CAPEX/OPEX modeling. Developed 3D renderings to visualize operational flow.",
       url: "https://drive.google.com/file/d/1OBBWGtfF-LMd24RUzQPVYStHeWudXoWE/view",
-    },
-    {
-      id: "tlfp-warehouse-development",
-      name: "The Lost Food Project (TLFP) Central Warehouse Development",
-      createdAt: "2023-03-19T00:00:00Z",
-      description: "Led 2D & 3D warehouse layout designs and project management for a 10,000+ sq. ft. charity facility. Successfully increased pallet capacity by 300%.",
-      url: "https://www.linkedin.com/feed/update/urn:li:activity:7103392957772267522/",
-    },
-    {
-      id: "fpsc-thermal-enhancement",
-      name: "Thermal Enhancement via Design Optimization (FPSC Research Article)",
-      createdAt: "2022-05-01T00:00:00Z",
-      description: "Published research on optimizing flat plate solar collectors. Utilized ANSYS Fluent for mathematical modeling, achieving a 79.32% thermal efficiency and 25.68% lower pressure drop compared to conventional models.",
-      url: "https://www.linkedin.com/posts/ngohyongming_design-solarenergy-heat-activity-6934755664581816320-_Nrp",
     },
   ],
 };
@@ -398,26 +302,17 @@ const contactPageData = {
   contactSection: {
     title: "Contact Me",
     profile_image_path: "your_profile_photo.png",
-    description:
-      "I am open to technical discussions regarding warehouse automation, intralogistics strategy, and 3D industrial simulations. Connect with me on LinkedIn or reach out via email for regional project collaborations.",
+    description: "I am open to technical discussions regarding warehouse automation and 3D industrial simulations.",
   },
-  blogSection: {
-    title: "",
-    subtitle:
-      "",
-    link: "",
-    avatar_image_path: "",
-  },
+  blogSection: { title: "", subtitle: "", link: "", avatar_image_path: "" },
   addressSection: {
     title: "Primary Location",
-    subtitle: "Kuala Lumpur, Malaysia (Managing SEAO/APAC Region)",
+    subtitle: "Kuala Lumpur, Malaysia",
     locality: "Kuala Lumpur",
     country: "Malaysia",
     region: "SEA",
-    postalCode: "",
-    streetAddress: "",
     avatar_image_path: "address_image.svg",
-    location_map_link: "https://www.google.com/maps/place/Kuala+Lumpur",
+    location_map_link: "https://maps.app.goo.gl/x8TzHEx6Z6hS96Xp8",
   },
   phoneSection: {
     title: "Contact Number",
